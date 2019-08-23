aa1abefe86f0c1bcbdf18e28fc5c7b29e45e9a4b
Kugatov Maxim <maximkugatov@gmail.com>
2019-08-23 17:56:48 +0300

    ch 3

--- src/com/kugmax/learn/scala/datastructure/List.scala
+++ src/com/kugmax/learn/scala/datastructure/List.scala
@@ -5,16 +5,9 @@ case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
-  def sum(ints: List[Int]): Int = ints match {
-    case Nil => 0
-    case Cons(x, xs) => x + sum(xs)
-  }
+  def sum(ints: List[Int]): Int = foldLeft(ints, 0)(_+_)
 
-  def product(ds: List[Double]): Double = ds match {
-    case Nil => 1.0
-    case Cons(0.0, _) => 0.0
-    case Cons(x, xs) => x * product(xs)
-  }
+  def product(ds: List[Double]): Double = foldLeft(ds, 1.0)(_*_)
 
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
@@ -39,19 +32,51 @@ object List {
     foldRight(ns, 1.0)(_ * _)
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, xs) => xs
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Cons(h, Nil)
+    case Cons(x, xs) => Cons(h, xs)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, xs) =>
+      def loop(count: Int, lInner: List[A]): List[A] = {
+        if (count == 0) lInner
+        else loop(count - 1, tail(lInner))
+      }
+      loop(n, l)
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(x: A, xs: List[A]) =>
+      if (f(x)) dropWhile(xs, f)
+      else xs
+  }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, Nil) => Nil
+    case Cons(x, xs) =>
+      xs match {
+        case Cons(_, ys) => Cons(x, xs)
+        case Cons(_, Nil) => Cons(x, Nil)
+      }
+  }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = foldRight(l, 0) ((_, b) =>  b +1)
 
-  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def length2[A](l: List[A]): Int = foldLeft(l, 0) ((b, a) =>  b +1)
+
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
 
   def map[A, B](l: List[A])(f: A => B): List[B] = ???
 }
---
+++ src/com/kugmax/learn/scala/datastructure/Runner.scala
@@ -0,0 +1,26 @@
+package com.kugmax.learn.scala.datastructure
+
+object Runner {
+  def main(args: Array[String]): Unit = {
+
+//
+//    System.out.println(List.foldRight(List(1,2,3), Nil:List[Int])(Cons(_,_)))
+//
+//    System.out.println( List.length( List(1, 1, 1, -1) ) )
+//    System.out.println( List.length( List(1) ) )
+//    System.out.println( List.length( List(5, 6) ) )
+//    System.out.println( List.length( List() ) )
+//
+
+//    System.out.println( List.foldLeft( List(0, 1), 0)(_ + _) )
+
+//    System.out.println( List.sum( List(0, 1, -1 , 5) ) )
+//    System.out.println( List.product( List(2, 3) ) )
+
+      System.out.println( List.length( List(1, 1, 1, -1) ) )
+      System.out.println( List.length( List(1) ) )
+      System.out.println( List.length( List(5, 6) ) )
+      System.out.println( List.length( List() ) )
+
+  }
+}
