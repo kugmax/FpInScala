49c11002a9c297ffc9b5af9fe2afa8d0a9fcaf5c
Maxim Kugatov <maximkugatov@gmail.com>
2021-01-27 09:56:00 +0200

    feat: testing progress

--- src/com/kugmax/learn/scala/testing/Gen.scala
+++ src/com/kugmax/learn/scala/testing/Gen.scala
@@ -1,7 +1,82 @@
 package com.kugmax.learn.scala.testing
 
-import com.kugmax.learn.scala.purlystate.SimpleRNG.{boolean, double, nonNegativeInt}
+import com.kugmax.learn.scala.purlystate.SimpleRNG.{double, nonNegativeInt, unit}
 import com.kugmax.learn.scala.purlystate.{RNG, SimpleRNG, State}
+
+object Prop {
+  type SuccessCount = Int
+  type TestCases = Int
+  type MaxSize = Int
+  type FailedCase = String
+
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+
+  case object Passed extends Result {
+    def isFalsified = false
+  }
+
+  case class Falsified(failure: FailedCase, successes: SuccessCount) extends Result {
+    def isFalsified = true
+  }
+
+  case class Prop(run: (MaxSize,TestCases,RNG) => Result) {
+    def &&(p: Prop): Prop = Prop(
+      (size, t, rng) =>
+        run(size, t, rng) match {
+          case Passed => p.run(size, t, rng)
+          case x => x
+        }
+      )
+
+    def ||(p: Prop): Prop = Prop(
+      (size, t, rng) =>
+        run(size, t, rng) match {
+          case Falsified(msg, _) => p.tag(msg).run(size, t, rng)
+          case x => x
+        }
+      )
+
+    def tag(msg: String): Prop = Prop {
+      (size, n,rng) => run(size, n, rng) match {
+        case Falsified(e, c) => Falsified(msg + "\n" + e, c)
+        case x => x
+      }
+    }
+  }
+
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+    s"generated an exception: ${e.getMessage}\n" +
+    s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (size,n,rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  def forAll[A](g: SGen[A])(f: A => Boolean): Prop = forAll(g(_))(f)
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+    (max,n,rng) =>
+      val casesPerSize = (n - 1) / max + 1
+      val props: Stream[Prop] = Stream.from(0).take((n min max) + 1).map(i => forAll(g(i))(f))
+
+      val prop: Prop =
+        props.map(p => Prop { (max, n, rng) =>
+          p.run(max, casesPerSize, rng)
+        }).toList.reduce(_ && _)
+      prop.run(max,n,rng)
+  }
+
+}
 
 case class Gen[A](sample: State[RNG,A]) {
 
@@ -18,9 +93,17 @@ case class Gen[A](sample: State[RNG,A]) {
   def listOfN(size: Gen[Int]): Gen[List[A]] =
     size.flatMap(n => Gen(State.sequence(List.fill(n)(sample))))
 
+  def listOfN(n: Int): Gen[List[A]] = Gen.listOfN(n, this)
+
+  def unsized: SGen[A] = SGen(_ => this)
+}
+
+case class SGen[A](g: Int => Gen[A]) {
+  def apply(n: Int): Gen[A] = g(n)
 }
 
 object Gen {
+
   def choose(start: Int, stopExclusive: Int): Gen[Int] =
     Gen(State(nonNegativeInt).map(n => start + n % (stopExclusive-start)))
 
@@ -37,14 +120,15 @@ object Gen {
 
   def _weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
     val g1Threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
-
     Gen(State(SimpleRNG.double).flatMap(d => if (d < g1Threshold) g1._1.sample else g2._1.sample))
   }
+
+  def listOf[A](g: Gen[A]): SGen[List[A]] = SGen( n => g.listOfN(n) )
 
   def main(args: Array[String]): Unit = {
     val rng = SimpleRNG(13)
 
-    println(choose(0.5, 0.9).sample.run(rng) )
+//    println(choose(0.5, 0.9).sample.run(rng) )
 //    println(unit(99).sample.run(rng) )
 //    println(boolean.sample.run(rng) )
 //
@@ -56,5 +140,11 @@ object Gen {
 //    println(choose(1, 9).listOfN(unit(2)).sample.run(rng) )
 
 //    println(union(unit(1), unit(2) ).sample.run(rng) )
+
+    val result = Prop.forAll(choose(1, 10))(v => true) && Prop.forAll(unit(3))(v => v == 3)
+//    val result = Prop.forAll(choose(1, 10))(v => false) || Prop.forAll(unit(3))(v => v != 3)
+
+
+    println( result.run(10, 4, rng) )
   }
 }
