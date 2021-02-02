c2662a777a68c9d0c7abe288d7e7c1075293ccb5
Maxim Kugatov <maximkugatov@gmail.com>
2021-02-02 11:19:20 +0200

    done with testing

--- src/com/kugmax/learn/scala/purlystate/RNG.scala
+++ src/com/kugmax/learn/scala/purlystate/RNG.scala
@@ -15,6 +15,15 @@ case class SimpleRNG(seed: Long) extends RNG {
 }
 
 object SimpleRNG {
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
+      val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
+      val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
+      (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+    }
+  }
+
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (value, nextRng) = rng.nextInt
     if (value < 0) (-(value + 1), nextRng) else (value, nextRng)
--- src/com/kugmax/learn/scala/testing/Gen.scala
+++ src/com/kugmax/learn/scala/testing/Gen.scala
@@ -1,7 +1,13 @@
 package com.kugmax.learn.scala.testing
 
+import java.util.concurrent.{ExecutorService, Executors}
+
+import com.kugmax.learn.scala.concurrent.Par
+import com.kugmax.learn.scala.concurrent.Par.Par
 import com.kugmax.learn.scala.purlystate.SimpleRNG.{double, nonNegativeInt, unit}
 import com.kugmax.learn.scala.purlystate.{RNG, SimpleRNG, State}
+import com.kugmax.learn.scala.testing.Gen.{choose, unit, weighted}
+import com.kugmax.learn.scala.testing.Prop.{check, checkPar, equal, forAll, run}
 
 object Prop {
   type SuccessCount = Int
@@ -21,11 +27,15 @@ object Prop {
     def isFalsified = true
   }
 
+  case object Proved extends Result {
+    override def isFalsified: Boolean = false
+  }
+
   case class Prop(run: (MaxSize,TestCases,RNG) => Result) {
     def &&(p: Prop): Prop = Prop(
       (size, t, rng) =>
         run(size, t, rng) match {
-          case Passed => p.run(size, t, rng)
+          case Passed | Proved => p.run(size, t, rng)
           case x => x
         }
       )
@@ -76,9 +86,53 @@ object Prop {
       prop.run(max,n,rng)
   }
 
+  def run(p: Prop, maxSize: Int = 100, testCases: Int = 100, rng: RNG = SimpleRNG.Simple(System.currentTimeMillis)): Unit =
+    p.run(maxSize, testCases, rng) match {
+      case Falsified(msg, n) =>
+        println(s"! Falsified after $n passed tests:\n $msg")
+      case Passed =>
+        println(s"+ OK, passed $testCases tests.")
+      case Proved =>
+        println(s"+ OK, proved property.")
+    }
+
+//  def check(p: => Boolean): Prop = {
+//    lazy val result = p
+//    forAll(Gen.unit(()))(_ => result)
+//  }
+
+  def check(p: => Boolean): Prop = Prop { (_, _, _) =>
+    if (p) Passed else Falsified("()", 0)
+  }
+
+  def equal[A](p: Par[A], p2: Par[A]): Par[Boolean] =
+    Par.map2(p,p2)(_ == _)
+
+  val S: Gen[ExecutorService] = weighted(
+    choose(1,4).map(Executors.newFixedThreadPool) -> .75,
+    Gen.unit(Executors.newCachedThreadPool) -> .25) // `a -> b` is syntax sugar for `(a,b)`
+
+  def forAllPar[A](g: Gen[A])(f: A => Par[Boolean]): Prop =
+    forAll( S ** g ) { case s ** a => f(a)(s).get }
+
+  def checkPar(p: Par[Boolean]): Prop =
+    forAllPar(Gen.unit(()))(_ => p)
+
+}
+
+object ** {
+  def unapply[A,B](p: (A,B)) = Some(p)
 }
 
 case class Gen[A](sample: State[RNG,A]) {
+
+  def **[B](g: Gen[B]): Gen[(A,B)] =
+    (this map2 g)((_,_))
+
+  def map[B](f: A => B): Gen[B] = Gen(sample.map(f))
+
+  def map2[B,C](g: Gen[B])(f: (A,B) => C): Gen[C] =
+    Gen(sample.map2(g.sample)(f))
 
   def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(State(
     s => {
@@ -100,6 +154,9 @@ case class Gen[A](sample: State[RNG,A]) {
 
 case class SGen[A](g: Int => Gen[A]) {
   def apply(n: Int): Gen[A] = g(n)
+
+  def map[B](f: A => B): SGen[B] =
+    SGen { g(_) map f }
 }
 
 object Gen {
@@ -118,33 +175,76 @@ object Gen {
 
   def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap(b => if(b) g1 else g2 )
 
-  def _weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
+  def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
     val g1Threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
     Gen(State(SimpleRNG.double).flatMap(d => if (d < g1Threshold) g1._1.sample else g2._1.sample))
   }
 
   def listOf[A](g: Gen[A]): SGen[List[A]] = SGen( n => g.listOfN(n) )
 
+  def listOf1[A](g: Gen[A]): SGen[List[A]] = SGen( _ => g.listOfN(1) )
+
+  def genStringIntFn(g: Gen[Int]): Gen[String => Int] =
+    g map (i => (s => i))
+
   def main(args: Array[String]): Unit = {
-    val rng = SimpleRNG(13)
+//    val rng = SimpleRNG(13)
 
-//    println(choose(0.5, 0.9).sample.run(rng) )
-//    println(unit(99).sample.run(rng) )
-//    println(boolean.sample.run(rng) )
+    //    println(choose(0.5, 0.9).sample.run(rng) )
+    //    println(unit(99).sample.run(rng) )
+    //    println(boolean.sample.run(rng) )
+    //
+    //    println(listOfN(7, choose(1, 9) ).sample.run(rng) )
+
+    //    println(boolean.flatMap( b => unit(99) ).sample.run(rng)  )
+    //    println(boolean._flatMap( b => unit(99) ).sample.run(rng)  )
+
+    //    println(choose(1, 9).listOfN(unit(2)).sample.run(rng) )
+
+    //    println(union(unit(1), unit(2) ).sample.run(rng) )
+
+    //    val result = Prop.forAll(choose(1, 10))(v => true) && Prop.forAll(unit(3))(v => v == 3)
+    //    val result = Prop.forAll(choose(1, 10))(v => false) || Prop.forAll(unit(3))(v => v != 3)
+    //    println( result.run(10, 4, rng) )
+
+    val smallInt = Gen.choose(-10, 10)
+//    val maxProp = Prop.forAll(listOf1(smallInt)) { ns =>
+//      val max = ns.max
+//      !ns.exists(_ > max)
+//    }
+//    Prop.run(maxProp)
+
+//    val sortedProp = forAll(listOf(smallInt)) { l =>
+//      val ls = l.sorted
+//      l.isEmpty || ls.tail.isEmpty || !ls.zip(ls.tail).exists { case (a,b) => a > b }
+//    }
 //
-//    println(listOfN(7, choose(1, 9) ).sample.run(rng) )
+//    Prop.run(sortedProp)
 
-//    println(boolean.flatMap( b => unit(99) ).sample.run(rng)  )
-//    println(boolean._flatMap( b => unit(99) ).sample.run(rng)  )
+    val ES: ExecutorService = Executors.newCachedThreadPool
+//    val p2 = Prop.check {
+//      val p = Par.map(Par.unit(1))(_ + 1)
+//      val p2 = Par.unit(2)
+//      p(ES).get == p2(ES).get
+//    }
+//
+//    run(p2)
 
-//    println(choose(1, 9).listOfN(unit(2)).sample.run(rng) )
+//    val p3 = check {
+//      equal(
+//        Par.map(Par.unit(1))(_ + 1),
+//        Par.unit(2)
+//      )(ES).get
+//    }
+//    run(p3)
 
-//    println(union(unit(1), unit(2) ).sample.run(rng) )
+    val p2 = checkPar {
+      equal (
+        Par.map(Par.unit(1))(_ + 1),
+        Par.unit(2)
+      )
+    }
+    run(p2)
 
-    val result = Prop.forAll(choose(1, 10))(v => true) && Prop.forAll(unit(3))(v => v == 3)
-//    val result = Prop.forAll(choose(1, 10))(v => false) || Prop.forAll(unit(3))(v => v != 3)
-
-
-    println( result.run(10, 4, rng) )
   }
 }
