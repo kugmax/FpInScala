eb84bcfb32a1555e1147346539fba71401c30a96
Maxim Kugatov <maximkugatov@gmail.com>
2021-02-06 16:26:43 +0200

    feat: progress parser

---
+++ src/com/kugmax/learn/scala/parsing/JSON.scala
@@ -0,0 +1,18 @@
+package com.kugmax.learn.scala.parsing
+
+trait JSON
+object JSON {
+
+  case object JNull extends JSON
+
+  case class JNumber(get: Double) extends JSON
+
+  case class JString(get: String) extends JSON
+
+  case class JBool(get: Boolean) extends JSON
+
+  case class JArray(get: IndexedSeq[JSON]) extends JSON
+
+  case class JObject(get: Map[String, JSON]) extends JSON
+
+}
--- src/com/kugmax/learn/scala/parsing/Parsers.scala
+++ src/com/kugmax/learn/scala/parsing/Parsers.scala
@@ -2,17 +2,32 @@ package com.kugmax.learn.scala.parsing
 import com.kugmax.learn.scala.testing.Prop._
 import com.kugmax.learn.scala.testing._
 
+import scala.util.matching.Regex
+
 trait Parsers[ParseError, Parser[+_]] { self =>
+
+//  primitives:
+  implicit def string(s: String): Parser[String]
+  implicit def regex(r: Regex): Parser[String]
+  def slice[A](p: Parser[A]): Parser[String]
+//  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
+  def succeed[A](a: A): Parser[A]
+  //  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
+  def flatMap[A,B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+
   def run[A](p: Parser[A])(input: String): Either[ParseError,A]
 
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+//  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
 
-  implicit def string(s: String): Parser[String]
   implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
 
-  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] 
-//    map2()
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] =
+    if (n <= 0) succeed(List())
+    else map2(p, listOfN(n-1, p))(_ :: _)
 
   def many[A](p: Parser[A]): Parser[List[A]] =
     map2(p, many(p))(_ :: _) or succeed(List())
@@ -20,21 +35,22 @@ trait Parsers[ParseError, Parser[+_]] { self =>
   def many1[A](p: Parser[A]): Parser[List[A]] =
     map2(p, many(p))(_ :: _)
 
-  def map[A,B](a: Parser[A])(f: A => B): Parser[B]
+  def map[A,B](p: Parser[A])(f: A => B): Parser[B] =
+    p.flatMap(a => succeed(f(a)))
 
   def map2[A,B,C](p1: Parser[A], p2: Parser[B])(f: (A,B) => C): Parser[C] =
     product(p1, p2).map(ab => f(ab._1, ab._2))
 
+  def _map2[A,B,C](p1: Parser[A], p2: Parser[B])(f: (A,B) => C): Parser[C] =
+    p1.flatMap(a => p2.map(b => f(a, b)))
+
   def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
 
-  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
-
-  def slice[A](p: Parser[A]): Parser[String]
-
-  def product[A,B](p: Parser[A], p2: Parser[B]): Parser[(A,B)]
+  def product[A,B](p: Parser[A], p2: Parser[B]): Parser[(A,B)] =
+    p.flatMap(a => p2.map(b => (a, b)))
 
   case class ParserOps[A](p: Parser[A]) {
-    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p,p2)
+    def |[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2)
     def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2)
     def many: Parser[List[A]] = self.many(p)
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
@@ -42,6 +58,7 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     def slice: Parser[String] = self.slice(p)
     def product[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
     def **[B](p2: Parser[B]): Parser[(A,B)] = self.product(p, p2)
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
   }
 
   object Laws {
