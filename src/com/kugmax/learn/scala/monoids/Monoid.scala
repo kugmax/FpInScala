1688ff349407e5eb8d31fb5af5712303b7f6318d
Maxim Kugatov <maximkugatov@gmail.com>
2021-02-09 10:59:04 +0200

    feat: monoid progress

--- src/com/kugmax/learn/scala/monoids/Monoid.scala
+++ src/com/kugmax/learn/scala/monoids/Monoid.scala
@@ -2,6 +2,7 @@ package com.kugmax.learn.scala.monoids
 
 import com.kugmax.learn.scala.concurrent.Par
 import com.kugmax.learn.scala.concurrent.Par.{Par, map2}
+import com.kugmax.learn.scala.datastructure.Tree
 import com.kugmax.learn.scala.testing.Gen
 import com.kugmax.learn.scala.testing.Prop.{Prop, checkPar, equal, forAll, run}
 
@@ -84,17 +85,87 @@ object Monoid {
 //      foldMapV(bs, par(m))(b => Par.lazyUnit(b))
 //    }
 
-  def isOrdering(v: IndexedSeq[Int]): Boolean = {
-
-    val orderingMonoid = new Monoid[ (Int, Int) ] {
-      override def op(a1: (Int, Int), a2: (Int, Int)): (Int, Int) = ???
-
-      override def zero: (Int, Int) = (0, 0)
+  def isOrdered(ints: IndexedSeq[Int]): Boolean = {
+    val mon = new Monoid[Option[(Int, Int, Boolean)]] {
+      def op(o1: Option[(Int, Int, Boolean)], o2: Option[(Int, Int, Boolean)]) =
+        (o1, o2) match {
+          // The ranges should not overlap if the sequence is ordered.
+          case (Some((x1, y1, p)), Some((x2, y2, q))) =>
+            Some((x1 min x2, y1 max y2, p && q && y1 <= x2))
+          case (x, None) => x
+          case (None, x) => x
+        }
+      val zero = None
     }
-
-    foldMapV(v, orderingMonoid)( a => true )
-
+    // The empty sequence is ordered, and each element by itself is ordered.
+    foldMapV(ints, mon)(i => Some((i, i, true))).map(_._3).getOrElse(true)
   }
+
+  sealed trait WC
+  case class Stub(chars: String) extends WC
+  case class Part(lStub: String, words: Int, rStub: String) extends WC
+
+  val wcMonoid: Monoid[WC] = new Monoid[WC] {
+    override def op(a1: WC, a2: WC): WC = (a1, a2) match {
+      case (Stub(l), Stub(r)) => Stub(l + r)
+      case (Part(ll, lw, lr), Stub(r)) => Part(ll, lw, lr+r)
+      case (Stub(l), Part(rl, rw, rr)) => Part(l+rl, rw, rr)
+      case (Part(ll, lw, lr), Part(rl, rw, rr)) => Part(ll, lw + (if ((lr + rl).isEmpty) 0 else 1) + rw, rr)
+    }
+    override def zero: WC = Stub("")
+  }
+
+  def count(s: String): Int = {
+
+    def wc(c: Char): WC =
+      if (c.isWhitespace)
+        Part("", 0, "")
+      else
+        Stub(c.toString)
+
+    def unstub(s: String) = s.length min 1
+
+    foldMapV(s.toIndexedSeq, wcMonoid)(wc) match {
+      case Stub(s) => unstub(s)
+      case Part(l, w, r) => unstub(l) + w + unstub(r)
+    }
+  }
+
+  trait Foldable[F[_]] {
+    def foldRight[A,B](as: F[A])(z: B)(f: (A,B) => B): B
+    def foldLeft[A,B](as: F[A])(z: B)(f: (B,A) => B): B
+    def foldMap[A,B](as: F[A])(f: A => B)(mb: Monoid[B]): B
+
+    def concatenate[A](as: F[A])(m: Monoid[A]): A =
+      foldLeft(as)(m.zero)(m.op)
+  }
+
+  val foldableList = new Foldable[List] {
+    override def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B = as.foldRight(z)(f)
+    override def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B = as.foldLeft(z)(f)
+    override def foldMap[A, B](as: List[A])(f: A => B)(mb: Monoid[B]): B = as.foldRight(mb.zero)((a,b) => mb.op(f(a), b))
+  }
+
+  val foldableIndexedSeq = new Foldable[IndexedSeq] {
+    override def foldRight[A, B](as: IndexedSeq[A])(z: B)(f: (A, B) => B): B = as.foldRight(z)(f)
+    override def foldLeft[A, B](as: IndexedSeq[A])(z: B)(f: (B, A) => B): B = as.foldLeft(z)(f)
+    override def foldMap[A, B](as: IndexedSeq[A])(f: A => B)(mb: Monoid[B]): B = as.foldRight(mb.zero)((a,b) => mb.op(f(a), b))
+  }
+
+  val foldableStream = new Foldable[Stream] {
+    override def foldRight[A, B](as: Stream[A])(z: B)(f: (A, B) => B): B = as.foldRight(z)(f)
+    override def foldLeft[A, B](as: Stream[A])(z: B)(f: (B, A) => B): B = as.foldLeft(z)(f)
+    override def foldMap[A, B](as: Stream[A])(f: A => B)(mb: Monoid[B]): B = as.foldRight(mb.zero)((a,b) => mb.op(f(a), b))
+  }
+
+  val foldableTree = new Foldable[Tree] {
+    override def foldRight[A, B](as: Tree[A])(z: B)(f: (A, B) => B): B = ???
+
+    override def foldLeft[A, B](as: Tree[A])(z: B)(f: (B, A) => B): B = ???
+
+    override def foldMap[A, B](as: Tree[A])(f: A => B)(mb: Monoid[B]): B = ???
+  }
+
 
   def main(args: Array[String]): Unit = {
 //    run(monoidLaws(intAddition, Gen.choose(0, 6)))
@@ -107,9 +178,12 @@ object Monoid {
 //    val l = words.foldLeft(stringMonoid.zero)(stringMonoid.op)
 //    println(l)
 
-    println(isOrdering(IndexedSeq(1, 2, 3)))
-    println(isOrdering(IndexedSeq(3, 2, 1)))
-    println(isOrdering(IndexedSeq(2, 1, 3)))
+//    println(isOrdered(IndexedSeq(1, 2, 3)))
+//    println(isOrdered(IndexedSeq(3, 2, 1)))
+//    println(isOrdered(IndexedSeq(2, 1, 3)))
+
+    run(monoidLaws(wcMonoid, Gen.unit("lorem ipsum do")))
+//    run(monoidLaws(wcMonoid, Gen.unit("lor sit amet, ")))
   }
 
  }
